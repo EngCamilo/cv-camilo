18a8a61aa0dcefbb2278ca498f146e446b6a9de6
Camilo Contreras <camilo@MacBook-Air-de-Camilo.local>
2025-05-30 18:02:08 -0500

    Actualización css

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900 font-sans">
       <Header />
-      <main className="cmax-w-5xl mx-auto px-6 py-10">
+      <main className="max-w-5xl mx-auto px-6 py-10">
         <Education />
         <Experience />
         <Skills />
