177b745a05c971a134bfbbfa8867e22d4632b51e
Camilo Contreras <camilo@MacBook-Air-de-Camilo.local>
2025-07-20 20:21:50 -0500

    Actualización Profile

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -5,7 +5,7 @@ export default function ProfileImage() {
     <div className="absolute left-36 md:left-1/3 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div className="w-52 h-52 md:w-60 md:h-60 mx-auto rounded-full border-4 border-yellow-500 shadow-lg overflow-hidden transform transition duration-500 hover:scale-105 hover:shadow-2xl bg-white">
         <img
-            src="https://photos.google.com/share/AF1QipPF5L05HsjXxPti7hQay8vdpA1sspDhsXCc6HhdHMQ-YK8rtUIM7bHxEu22oZWzaQ/photo/AF1QipMBt3NS9D6i4ULro74QhgAcMSxvIWHh8gzSqbNG?key=bjdSMWNjN053bWdobW9VT3VHWkY1M2NFcHQ2d05R"
+            src="./foto.jpg"
             alt="Foto de perfil"
             className="w-full h-full object-cover"
         />
